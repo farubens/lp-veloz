620218dbbe738918169a4fbe45c7a540e8d9dca6
farubens <fa.rubens@gmail.com>
2020-12-07 10:13:28 -0300

    esta é a primeira atualização

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
   return (
     <div>
       <h1>Olá mundo do js</h1>
-      <p> Este é um teste de ual next.js</p>
+      <p> Esta é a primeira atualização</p>
     </div>
     )
 }
